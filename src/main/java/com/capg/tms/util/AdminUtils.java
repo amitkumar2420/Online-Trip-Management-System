f1cba84621d05a507c2c270ec35d3dce9eda0206
Abhinav-beepp <abhinavprakash413@gmail.com>
2021-04-02 11:32:11 +0530

    seventh commit

--- src/main/java/com/capg/tms/util/AdminUtils.java
+++ src/main/java/com/capg/tms/util/AdminUtils.java
@@ -1,14 +1,35 @@
 package com.capg.tms.util;
 
+import java.util.ArrayList;
+import java.util.List;
+import com.capg.tms.entities.Admin;
+import com.capg.tms.model.AdminDTO;
 
-
-
-
-	public class AdminUtils {
+public  class AdminUtils {
 		
+		public static List<AdminDTO> convertToAdminDTOList(List<Admin> list)
+		{
+			List<AdminDTO> dtolist = new ArrayList<AdminDTO>();
+			for(Admin admin : list)
+				dtolist.add(convertToAdminDto(admin));
+			return dtolist;
+			
+		}
 		
+		public static Admin convertToAdmin(AdminDTO dto)
+		{
+			Admin admin = new Admin();
+			admin.setAdminId(dto.getAdminId());
+			admin.setPassword(dto.getPassword());
+			return admin;
+		}
 		
-		
+		public static AdminDTO convertToAdminDto(Admin admin) {
+			AdminDTO dto = new AdminDTO();
+			dto.setAdminId(admin.getAdminId());
+			dto.setPassword(admin.getPassword());
+			return dto;
+		}
 		
 		
 
