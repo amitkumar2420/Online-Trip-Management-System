219e7a5497b5fb6f0513d6641aa30e0749fe75c9
Saptarsikundu <saptarsikundu17@gmail.com>
2021-04-03 23:14:25 +0530

    updated++

--- src/main/java/com/capg/tms/service/FeedbackServiceImpl.java
+++ src/main/java/com/capg/tms/service/FeedbackServiceImpl.java
@@ -1,38 +1,58 @@
-package com.capg.tms.service;
-
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.capg.tms.entities.Feedback;
 import com.capg.tms.exceptions.CustomerNotFoundException;
 import com.capg.tms.exceptions.FeedbackNotFoundException;
 import com.capg.tms.model.FeedbackDTO;
-@Service
+import com.capg.tms.repository.IBookingRepository;
+import com.capg.tms.repository.IFeedbackRepository;
+import com.capg.tms.util.FeedbackUtils;
+
+
 public class FeedbackServiceImpl implements IFeedbackService {
+	@Autowired
+	IFeedbackRepository feedbackRepo;
 
 	@Override
 	public FeedbackDTO addFeedback(Feedback feedback) {
-		// TODO Auto-generated method stub
-		return null;
+		Feedback addFeedback=new Feedback();
+		addFeedback=feedbackRepo.save(feedback);
+		return FeedbackUtils.convertToFeedbackDto(addFeedback);
+		
 	}
 
 	@Override
 	public FeedbackDTO findByFeedbackId(int feedbackId) throws FeedbackNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		Feedback findbyid=new Feedback();
+		findbyid=feedbackRepo.getOne((int) feedbackId);
+		feedbackRepo.findById((int) feedbackId);
+		return FeedbackUtils.convertToFeedbackDto(findbyid);
+		
+		
 	}
 
 	@Override
 	public FeedbackDTO findByCustomerId(int customerId) throws CustomerNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		Feedback custid=new Feedback();
+		custid=feedbackRepo.getOne((int) customerId);
+		feedbackRepo.findById((int) customerId);
+		return FeedbackUtils.convertToFeedbackDto(custid);
+		
 	}
 
 	@Override
 	public List<FeedbackDTO> viewAllFeedbacks() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Feedback> getFeedback=new ArrayList<Feedback>();
+		 getFeedback=feedbackRepo.findAll();
+		 return FeedbackUtils.convertToFeedbackDtoList(getFeedback);
+		 
+		
+		
 	}
 
 }
+
