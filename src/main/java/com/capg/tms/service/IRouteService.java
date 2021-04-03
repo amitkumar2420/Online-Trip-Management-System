d016d4cd45b81a2d006fe9edecd83fbe364b6002
AvinashSankar <avinashsankar33@gmail.com>
2021-04-03 22:41:49 +0530

    Service update

--- src/main/java/com/capg/tms/service/IRouteService.java
+++ src/main/java/com/capg/tms/service/IRouteService.java
@@ -2,17 +2,20 @@ package com.capg.tms.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.capg.tms.entities.Route;
 import com.capg.tms.exceptions.RouteNotFoundException;
-
+import com.capg.tms.model.RouteDTO;
+@Service
 public interface IRouteService {
 	
 	
-	public  Route  addRoute(Route route);
-	public  Route  updateRoute(Route route) throws RouteNotFoundException;
-	public  Route   removeRoute(int routeId) throws RouteNotFoundException;
-	public  Route    searchRoute(int routeId) throws RouteNotFoundException;
-	public  List<Route> viewRouteList();
+	public  RouteDTO  addRoute(Route route);
+	public  RouteDTO  updateRoute(Route route) throws RouteNotFoundException;
+	public  RouteDTO   removeRoute(int routeId) throws RouteNotFoundException;
+	public  RouteDTO    searchRoute(int routeId) throws RouteNotFoundException;
+	public  List<RouteDTO> viewRouteList();
 	
 	
 	
---
+++ src/main/java/com/capg/tms/service/RouteServiceImpl.java
@@ -0,0 +1,53 @@
+package com.capg.tms.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.capg.tms.entities.Route;
+import com.capg.tms.exceptions.RouteNotFoundException;
+import com.capg.tms.model.RouteDTO;
+import com.capg.tms.repository.IRouteRepository;
+import com.capg.tms.util.RouteUtils;
+
+public class RouteServiceImpl implements IRouteService {
+	@Autowired
+	IRouteRepository routeRepo;
+
+	@Override
+	public RouteDTO addRoute(Route route) {
+		Route routeEntity = routeRepo.save(route);
+		return RouteUtils.convertToRouteDto(routeEntity);
+	}
+
+	@Override
+	public RouteDTO updateRoute(Route route) throws RouteNotFoundException {
+		Route routeEntity = routeRepo.save(route);
+		return RouteUtils.convertToRouteDto(routeEntity);
+	}
+
+	@Override
+	public RouteDTO removeRoute(int routeId) throws RouteNotFoundException {
+		Route routetemp = new Route();
+		routetemp = routeRepo.getOne((int)routeId);
+		routeRepo.deleteById((int)routeId);
+		return RouteUtils.convertToRouteDto(routetemp);
+	}
+
+	@Override
+	public RouteDTO searchRoute(int routeId) throws RouteNotFoundException {
+		Route routetemp = new Route();
+		routetemp = routeRepo.getOne((int)routeId);
+		routeRepo.findById((int)routeId);
+		return RouteUtils.convertToRouteDto(routetemp);
+	}
+
+	@Override
+	public List<RouteDTO> viewRouteList() {
+		List<Route>getRoute=new ArrayList<Route>();
+		getRoute=routeRepo.findAll();
+		return RouteUtils.convertToRouteDTOList(getRoute);
+	}
+
+}
